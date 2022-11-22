57b6c4477eb42135ec92ccb902d39c3f6bf63f29
olisbenna <olisbenna@gmail.com>
2022-11-22 12:19:10 +0000

    PID reglir

---
+++ SP.PID V2.0/Arduino_PID/Arduino_PID.ino
@@ -0,0 +1,191 @@
+#include <util/atomic.h> // For the ATOMIC_BLOCK macro
+#include <Wire.h>
+
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+#define OLED_DC     8
+#define OLED_CS     10
+#define OLED_RESET  9
+
+
+
+#define ENCA 2 // YELLOW
+#define ENCB 4 // WHITE
+#define PWM 5
+#define IN2 5
+#define IN1 3
+
+volatile int posi = 0; // specify posi as volatile: https://www.arduino.cc/reference/en/language/variables/variable-scope-qualifiers/volatile/
+long prevT = 0;
+float eprev = 0;
+float eintegral = 0;
+int target;
+String readString = "";
+float pwr;
+int dir;
+long time = micros();
+int set = 1;
+float ratio = 1440 / 360 ;
+
+float kp = 1;
+float kd = 0;
+float ki = 0;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(ENCA,INPUT);
+  pinMode(ENCB,INPUT);
+
+  
+  pinMode(PWM,OUTPUT);
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+  
+  Serial.println("target pos");}
+
+void loop() {
+  
+
+  // set target position
+  //int target = 1200;
+  while (Serial.available()) 
+  {
+    char c = Serial.read(); //gets one byte from serial buffer
+    readString += c; //makes the String readString
+    delay(2); //slow looping to allow buffer to fill with next character
+  }
+  int end = readString.length();
+
+  if (readString.length() >0) 
+  {
+    //Serial.println(readString); //so you can see the captured String
+    
+
+    if (readString.substring(0,1) == "p")
+    {
+      kp = readString.substring(1,end).toFloat();
+    }
+
+    else if (readString.substring(0,1) == "i")
+    {
+      ki = readString.substring(1,end).toFloat();
+    }
+
+    else if (readString.substring(0,1) == "d")
+    {
+      kd = readString.substring(1,end).toFloat();
+    }
+
+    else{
+      target = readString.toInt(); //convert readString into a number
+      target = target * ratio;
+    }
+    readString = "";
+  }
+
+  // // PID constants
+  
+  // if (set == 1 && micros() - time > 3000000)
+  // {
+  //   Serial.println("up");
+  //   target = 1440;
+  //   set = 0;
+  //   time = micros();
+  // }
+
+  // if (set == 0 && micros() - time > 3000000)
+  // {
+  //   Serial.println("Down");
+  //   target = 0;
+  //   set = 1;
+  //   time = micros();
+  // }
+  
+
+  // time difference
+  long currT = micros();
+  float deltaT = ((float) (currT - prevT))/( 1.0e6 );
+  prevT = currT;
+
+  // Read the position in an atomic block to avoid a potential
+  // misread if the interrupt coincides with this code running
+  // see: https://www.arduino.cc/reference/en/language/variables/variable-scope-qualifiers/volatile/
+  int pos = 0; 
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+    pos = posi;
+  }
+  
+  // error
+  int e = pos - target;
+
+  // derivative
+  float dedt = (e-eprev)/(deltaT);
+
+  // integral
+  eintegral = eintegral + e*deltaT;
+
+  // control signal
+  float u = kp*e + kd*dedt + ki*eintegral;
+
+  // motor power
+  
+  if(e >= -1 && e <= 1)
+  {
+    pwr = 0;
+    dir = 0;
+  }
+  else{
+    
+    float pwr = fabs(u);
+    if( pwr > 255 ){
+        pwr = 255;
+        }
+    
+    
+    if(u<0){
+    dir = -1;
+    }
+    else{dir = 1;}
+  }
+
+  // signal the motor
+ setMotor(dir,pwr,PWM,IN1,IN2);
+
+
+  // store previous error
+  eprev = e;
+
+  Serial.print(target);
+  Serial.print(" ");
+  Serial.print(pos);
+  Serial.println();
+  delay(1);
+}
+
+void setMotor(int dir, int pwmVal, int pwm, int in1, int in2){
+  analogWrite(pwm,pwmVal);
+  if(dir == 1){
+    digitalWrite(in1,HIGH);
+    digitalWrite(in2,LOW);
+  }
+  else if(dir == -1){
+    digitalWrite(in1,LOW);
+    digitalWrite(in2,HIGH);
+  }
+  else{
+    digitalWrite(in1,LOW);
+    digitalWrite(in2,LOW);
+  }  
+}
+
+void readEncoder(){
+  int b = digitalRead(ENCB);
+  if(b > 0){
+    posi++;
+  }
+  else{
+    posi--;
+  }
+}
--- SP.PID V2.0/NEWGUI.py
+++ SP.PID V2.0/NEWGUI.py
@@ -8,38 +8,9 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import random
 import sys
-import board
-from adafruit_seesaw import seesaw, rotaryio, digitalio
-import time
-import digitalio as dg
-import pwmio
 from time import sleep, perf_counter
 from threading import Thread
 
-
-pulse = dg.DigitalInOut(board.D17)
-pulse.direction = dg.Direction.OUTPUT
-direc = dg.DigitalInOut(board.D15)
-direc.direction = dg.Direction.OUTPUT
-enable = dg.DigitalInOut(board.D18)
-enable.direction = dg.Direction.OUTPUT
-ms1 = dg.DigitalInOut(board.D20)
-ms1.direction = dg.Direction.OUTPUT
-ms2 = dg.DigitalInOut(board.D21)
-ms2.direction = dg.Direction.OUTPUT
-ms1.value = False
-ms2.value = False
-stop = dg.DigitalInOut(board.D9)
-stop.direction = dg.Direction.INPUT
-stop.pull = dg.Pull.UP
-e_stop_top = dg.DigitalInOut(board.D7)
-e_stop_top.direction = dg.Direction.INPUT
-e_stop_top.pull = dg.Pull.UP
-e_stop_bot = dg.DigitalInOut(board.D8)
-e_stop_bot.direction = dg.Direction.INPUT
-e_stop_bot.pull = dg.Pull.UP
-motor_Ain1 = pwmio.PWMOut(board.D19,frequency = 1000)
-motor_Ain2 = pwmio.PWMOut(board.D13,frequency = 1000)
 
 class Live():
     def __init__(self):
@@ -53,433 +24,22 @@ class Live():
             self.good = True
             #print(self.good)
  
-class SetPoint():
-    def __init__(self) -> None:
-        self.pi = 3.14159265359                                                         # Everybody knows pi
-        self.degrees_per_step = 1.8                                                     # for some reason this motor will move 0.225 degrees per step
-        self.diameter = 19.65                                                           # diameter in millimeters
-        self.total_travel = 820                                                         # total travel of the gantry plate is 800 millimeters
-        self.circumference = self.diameter * self.pi                                    # circumference in millimeters
-        self.steps_per_revolution = 360 / self.degrees_per_step                         # number of steps in a revolution
-        self.distance_per_step = self.circumference / self.steps_per_revolution         # distance per step in millimeters
-        self.steps_total = self.total_travel / self.distance_per_step                   # total number of steps per length of travel of gantry plate
-        self.rotation = False                                                           # variable for which direction the motor will spin
-        self.direction = "up"                                                           # variable to tell program to go up or down
-        self.current_position = 0                                                       # initializes in lowest position
-        self.highest_position = self.steps_total                                        # for readability define top position
-        self.lowest_position = 0                                                        # for readability define bottom position
-        self.disable_microstepping()
-        self.microstepping = False
-        self.reset_actuator()                                                           # actuator finds zero position (bottom)
 
-    def get_total_steps(self):
-        ''' Returns total number of steps for end to end travel '''
-        return self.steps_total
-
-    def enable_driver(self):        
-        ''' Function that enables Easydriver for use '''
-        enable.value = False
-
-    def disable_driver(self):        
-        ''' Function that disables Easydriver after use '''
-        enable.value = True 
-
-    def update_rotation(self,direction):
-        ''' Function updates variable for gantry travel direction '''
-        self.direction = direction
-        if self.direction == "up":
-            self.rotation = True            # False means actuator moves down
-        if self.direction == "down":
-            self.rotation = False
-        direc.value = self.rotation
-
-    def pulse(self):
-        ''' Function sends out one pulse on pulse pin '''
-        pulse.value = True
-        time.sleep(0.0007)      # wait
-        pulse.value = False
-        time.sleep(0.0007)      # wait
-
-    def update_position(self,distance):
-        ''' Function keeps track of current position of gantry '''
-        if self.direction == "down":
-            if self.microstepping:
-                self.current_position -= distance/8
-            else:
-                self.current_position -= distance
-        if self.direction == "up":
-            if self.microstepping:
-                self.current_position += distance/8
-            else:
-                self.current_position += distance
-
-    def move(self,position):
-        ''' Function moves gantry to a requested position '''
-        steps = self.current_position - position
-        if self.current_position == position:
-            return         
-        if self.current_position > position:
-            self.update_rotation("down")
-        if self.current_position < position:
-            self.update_rotation("up")
-            steps = -steps
-        self.spin_motor(steps)
-        #print("Current position " + str(self.current_position))
-
-        
-
-    def spin_motor_helper(self,steps):
-        ''' Function that moves the stepper motor '''
-        self.enable_driver()
-        for _ in range(0, int(steps)):
-            self.pulse()                    # one high and one low on pin
-            self.update_position(1)
-            if self.current_position >= self.highest_position:  # stops before top end of profile
-                break
-            if self.current_position <= self.lowest_position:   # stops before bottom end of profile
-                break                 
-        self.disable_driver()
-
-    def spin_motor(self,steps):
-        if steps <= 10:
-            #print("Microstepping")
-            self.enable_microstepping()
-            self.spin_motor_helper(steps*8)
-            self.disable_microstepping()
-        else:
-            self.spin_motor_helper(steps)
-        self.current_position = int(round(self.current_position,0))
-
-    def enable_microstepping(self):
-        #print("Microstepping enabled!")
-        self.microstepping = True
-        ms1.value = True
-        ms2.value = True
-
-    def disable_microstepping(self):
-        #print("Microstepping disabled!")
-        self.microstepping = False
-        ms1.value = False
-        ms2.value = False       
-
-    def reset_actuator(self):
-        ''' Function that moves the gantry to zero position (bottom) '''
-        self.update_rotation("down")    # sets travel direction to down
-        self.enable_driver()            # enables the driver to move
-        while True:
-            self.pulse()                # sends out pulses until the button reads high
-            if stop.value == True: #GPIO.input(self.button_pin) == GPIO.HIGH: # read a button
-                #print("Bottom reached!")    
-                break                        # break loop when bottom reached
-        self.disable_driver()           # driver disabled
-
-class Encoders():
-    def __init__(self,address,zerostate=0):
-            self.SKREF = 2500000
-            self.qt_enc = seesaw.Seesaw(board.I2C(), addr=address)
-            self.qt_enc.pin_mode(24, self.qt_enc.INPUT_PULLUP)
-            self.button = digitalio.DigitalIO(self.qt_enc, 24)
-            self.button_held = False
-            self.encoder = rotaryio.IncrementalEncoder(self.qt_enc)
-            self.last_position = 0
-            self.pos = 0
-            self.zerostate = zerostate
-
-
-    def Get_enc_val(self):
-        self.pos = -self.encoder.position
-        if self.pos < 0 :
-            self.encoder.position = 0
-            self.pos = 0
-        if not self.button.value and not self.button_held:
-            self.encoder.position = self.zerostate
-            self.pos = self.zerostate
-            self.last_position = self.zerostate
-        if self.pos != self.last_position:
-            if abs(abs(self.pos)-abs(self.last_position)) < 100:
-                self.position = self.pos
-                self.last_position = self.position
-                return self.position/10
-            else:
-                return self.last_position/10
-        else:
-            return self.last_position/10
-
-    def get_setpoint(self):
-        self.pos = -self.encoder.position
-        if self.pos > self.SKREF and self.pos < (self.SKREF + 10):
-            self.encoder.position = -(self.SKREF)
-            self.pos = self.SKREF
-            self.last_position = self.SKREF
-        if self.pos < 0:
-            self.encoder.position = 0
-            self.pos =0
-        if self.pos != self.last_position:
-            if abs(abs(self.pos)-abs(self.last_position)) < 100:
-                self.position = self.pos
-                self.last_position = self.position
-                return self.position
-            else:
-                return self.last_position
-        else:
-            return self.last_position
-
-class get_values_thread():
-    def __init__(self):
-        self.running =True
-    
-    def terminate(self):
-        self.running = False
-    
-    def run(self):
-        kpold = kp.Get_enc_val()
-        spold = sp.get_setpoint()
-        kiold = ki.Get_enc_val()
-        kdold = kd.Get_enc_val() 
-        while True:
-            gv.sptemp = sp.get_setpoint() *10
-            #print(gv.sptemp)
-            kptemp = kp.Get_enc_val()
-            kitemp = ki.Get_enc_val()
-            kdtemp = kd.Get_enc_val()
-            #print(gv.sptemp)
-            if not sp.button.value and not sp.button_held:
-                gv.sp = gv.sptemp
-            if kpold != kptemp:
-                gv.kp = kptemp
-                kpold = kptemp
-                pid_regulator.set_p(kptemp)
-            if kiold != kitemp:
-                gv.ki = kitemp
-                kiold = kitemp
-                pid_regulator.set_i(kitemp)
-            if kdold != kdtemp:
-                gv.kd = kdtemp
-                kdold = kdtemp
-                pid_regulator.set_d(kdtemp)
-            time.sleep(0.05)
-
-class global_val():
-    def __init__(self):
-            self.POS = 0
-            self.dc_enc_val = 0
-            self.exit = False
-            self.sp = 0
-            self.kp = 0
-            self.ki = 0
-            self.kd = 0
-            self.sptemp = 0
-            self.PWM_val = 0
-            self.disttravel = 0
-
-class stepper_thread():
-    def __init__(self):
-        self._running = True
-
-    def terminate(self):
-        self._running = False
-
-    def run(self):
-        ratio = 2600/775
-        sp = gv.sp
-        interval = 0.1
-        old_time = time.time()
-        while True:
-            cur_time = time.time()
-            if cur_time - old_time >= interval:
-                sp = gv.sp
-                old_time = cur_time
-            if sp != actuator.current_position:
-                actuator.move(sp *ratio)
-            time.sleep(0.0001)
-
-class PID:
-    def __init__(self):
-        self.kp = 1
-        self.ki = 0
-        self.kd = 0
-        self.time = 0
-        self.old_time = 0
-        self.eprew = 0
-        self.eintegral = 0
-
-    def regulate(self,target,pos):
-        self.time = time.time()
-        time_delta = self.time - self.old_time
-        self.old_time = self.time
-        
-        e = pos - target
-
-        dedt = (e - self.eprew)/(time_delta)
-
-        self.eintegral = self.eintegral + (e * time_delta)
-        kp_val = self.kp*e
-        ki_val = self.ki*self.eintegral
-        kd_val = self.kd*dedt
-        u = kp_val + ki_val + kd_val
-        self.eprew = e
-        #print((e,self.eintegral,dedt,u,))
-        if u > 255:
-            u = 255
-        if u < -255:
-            u = -255
-        return u
-
-    def set_p(self,value):
-        self.kp = value
-
-    def set_i(self,value):
-        self.ki = value
-
-    def set_d(self,value):
-        self.kd= value
-
-class DC_encoders():
-    def __init__(self) -> None:
-        self.encA = dg.DigitalInOut(board.D14)
-        self.encA.direction = dg.Direction.INPUT
-        self.encA.pull = dg.Pull.DOWN
-        self.encB = dg.DigitalInOut(board.D16)
-        self.encB.direction = dg.Direction.INPUT
-        self.encB.pull = dg.Pull.DOWN
-        self.direction = ''
-        self.i = 0
-        self.iold = 0
-        self.last_enc = True
-
-    def read(self):
-        if self.encA.value == True and self.last_enc == False:
-            if self.encB.value == True:
-                self.direction = 'Forward'
-            else:
-                self.direction = 'Backwards'
-    
-            if self.direction == 'Forward':
-                self.i += 1
-            else:
-                self.i -= 1
-            self.last_enc = True
-            return
-
-        elif self.encA.value == False and self.last_enc == True:
-            self.last_enc = False
-            return
-
-        if self.i != self.iold:
-            self.iold = self.i
-        return
-
-    def get_pos(self):
-        self.read()
-        return self.i
-
-class DC_thread():
-    def __init__(self):
-        self._running = True
-
-    def terminate(self):
-        self._running = False
-
-    def run(self):
-        time.sleep(10)
-        if e_stop_top.value == False:
-            motor_Ain1.duty_cycle = 30000
-            motor_Ain2.duty_cycle = 0
-            while not e_stop_top.value:
-                time.sleep(0.00001)
-            dc_encoders.i = 0
-        else:
-            dc_encoders.i = 0
-        motor_Ain1.duty_cycle = 0
-        motor_Ain2.duty_cycle = 0
-        dc_encoders.i = 0
-        time.sleep(3)
-        dc_encoders.i = 0
-        motor_Ain1.duty_cycle = 0
-        motor_Ain2.duty_cycle = 30000
-        while e_stop_bot.value == False:
-            time.sleep(0.0001)
-         
-        motor_Ain1.duty_cycle = 0
-        motor_Ain2.duty_cycle = 0            
-        time.sleep(3)
-        gv.disttravel = abs(dc_encoders.i)
-        dc_encoders.i = 0 
-        ratio = gv.disttravel / 775
-        print(gv.disttravel)
-        while True:
-            PWM_val = pid_regulator.regulate(gv.sp * ratio, gv.dc_enc_val)
-            gv.PWM_val = PWM_val
-            #print(gv.sp, gv.dc_enc_val,PWM_val)
-            if PWM_val > 65000:
-                PWM_val = 65000
-
-            if PWM_val < -65000:
-                PWM_val = -65000
-
-            if PWM_val < 0 and e_stop_top.value == False:
-                motor_Ain1.duty_cycle = abs((PWM_val))*255
-                motor_Ain2.duty_cycle = 0
-            
-            elif PWM_val > 0 and e_stop_bot.value == False:
-                motor_Ain1.duty_cycle = 0
-                motor_Ain2.duty_cycle = abs(PWM_val)*255
-            else:
-                motor_Ain1.duty_cycle = 0
-                motor_Ain2.duty_cycle = 0
-            time.sleep(0.001)
-            
-class DC_encoder_thread():
-    def __init__(self):
-        self._running = True
-
-    def terminate(self):
-        self._running = False
-
-    def run(self):
-        while True:
-            gv.dc_enc_val = dc_encoders.get_pos()
-            time.sleep(0.0000001)
- 
-
-dc_encoders = DC_encoders()
-pid_regulator = PID()
 live = Live()
-actuator = SetPoint()
-kp = Encoders(0x38,1)
-ki = Encoders(0x37)
-kd = Encoders(0x36)
-sp = Encoders(0x3a)
-gv = global_val()
 
-get_values = get_values_thread()
-get_values = Thread(target= get_values.run)
-get_values.start()
-
-stepper = stepper_thread()
-stepper = Thread(target= stepper.run)
-stepper.start()
-
-DC_motor = DC_thread()
-DC_motor = Thread(target= DC_motor.run)
-DC_motor.start()
-
-DC_encoder_read = DC_encoder_thread()
-DC_encoder_read = Thread(target= DC_encoder_read.run)
-DC_encoder_read.start()
 
 def on_escape(event=None):
-    gv.exit = True
+    exit = True
     win.destroy()
     sys.exit()
     
 
 # update PID values from encoders
 def update():
-    label1['text'] = "K_p = " + str(gv.kp)
-    label2['text'] = "K_i = " + str(gv.ki)
-    label3['text'] = "K_d = " + str(gv.kd)
-    takki1['text'] = "SP = " + str(gv.sptemp)
+    label1['text'] = "K_p = " + str(1)
+    label2['text'] = "K_i = " + str(2)
+    label3['text'] = "K_d = " + str(3)
+    takki1['text'] = "SP = " + str(4)
     if live.good is True:
         takki1['bg'] = "green"
         takki1['activebackground'] = "green"
@@ -501,11 +61,11 @@ def init():
 # function for live animation on the BIG graph
 def Big_Plot(i, y1, y2):
 
-    y = gv.sp
+    y = random.randint(0,100)
     y1.append(y)
     y1 = y1[-x_len:]
 
-    y = gv.dc_enc_val * (gv.disttravel/775)
+    y = random.randint(100,200)
     y2.append(y)
     y2 = y2[-x_len:]
 
@@ -520,7 +80,7 @@ def Big_Plot(i, y1, y2):
 def small_plot1(i, ys):
 
     # Read temperature (Celsius) from TMP102
-    K_p = gv.PWM_val     
+    K_p = random.randint(0,50)     
 
     # Add y to list
     ys.append(K_p)
@@ -537,7 +97,7 @@ def small_plot1(i, ys):
 def small_plot2(i, ys):
 
     # Read temperature (Celsius) from TMP102
-    K_i = round(gv.ki)     
+    K_i = random.randint(0,50)    
 
 
     # Add y to list
@@ -555,7 +115,7 @@ def small_plot2(i, ys):
 def small_plot3(i, ys):
 
     # get data for D
-    K_d = round(gv.ki)     
+    K_d = random.randint(0,50)    
 
     # Add y to list
     ys.append(K_d)
@@ -573,6 +133,12 @@ x_range = [0,x_len]
 INTERVALS = 0
 
 # Create an instance of tkinter frame
+splash = Tk()
+splash.title("Test Loading screen")
+splash.geometry("1024x600")
+
+
+
 
 win= Tk()
 
@@ -588,7 +154,8 @@ win.wm_attributes("-topmost", True)
 # close window with key `ESC`
 win.bind("<Escape>", on_escape)
 # hide cursor
-win.config(cursor = "none")
+'''uncomment win.config to hide cursor'''
+#win.config(cursor = "none")
 # Set the window size
 win.geometry("1024x600")
 # Use TkAgg
@@ -693,7 +260,7 @@ frame3.grid(row =3, column =3, padx=10,pady=10)
 label3 = tk.Label(master=frame3)
 label3.pack(padx=3, pady=5)
 
-takki1 = tk.Button(master= win, command= live.switch)
+takki1 = tk.Button(master= win, command= None)
 takki1.grid(row =3, column =0)
 
 takki2 = tk.Button(master= win,activebackground= None, text= "Exit", command= on_escape)
@@ -701,5 +268,10 @@ takki2.grid(row =3, column =4)
 
 # run first time
 update()
+def winmain():
+    splash.destroy
+    win.mainloop()
 
-win.mainloop()
+splash.after(30000, winmain)
+
+splash.mainloop()
