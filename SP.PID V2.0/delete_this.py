81585caba8afcb7e4e2ee59fa037683ef4863f28
olisbenna <olisbenna@gmail.com>
2022-12-01 11:55:44 +0000

    d

--- SP.PID V2.0/delete_this.py
+++ SP.PID V2.0/delete_this.py
@@ -1,21 +1,38 @@
+from tkinter import *
+import tkinter as tk
+from unittest import TextTestRunner
+import matplotlib
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.gridspec import GridSpec
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import random
+import sys
+import time
+from threading import Thread
+from PIL import ImageTk, Image
 import csv
-  
-  
-# field names 
-fields = ['Name', 'Branch', 'Year', 'CGPA'] 
-    
-# data rows of csv file 
-rows = [ ['Nikhil', 'COE', '2', '9.0'], 
-         ['Sanchit', 'COE', '2', '9.1'], 
-         ['Aditya', 'IT', '2', '9.3'], 
-         ['Sagar', 'SE', '1', '9.5'], 
-         ['Prateek', 'MCE', '3', '7.8'], 
-         ['Sahil', 'EP', '2', '9.1']] 
-  
-with open('GFG.csv', 'w',newline= '') as f:
-      
-    # using csv.writer method from CSV package
-    write = csv.writer(f)
-      
-    write.writerow(fields)
-    write.writerows(rows)
+import shutil 
+from datetime import datetime
+from itertools import zip_longest
+import os
+
+def ewin_retry():
+    pass
+
+def ewin_destroy():
+    pass
+
+ewin = tk.Tk()
+ewin.columnconfigure([0,1], minsize=250)
+ewin.rowconfigure([0, 1], minsize=100)
+ewin.eval('tk::PlaceWindow . center')
+label1 = tk.Label(text="USB not found", font=("Helvetica", 20))
+label1.grid(row=0, column=0, columnspan=2)
+
+label2 = tk.Button(text="retry", command=ewin_retry)
+label2.grid(row=1, column=0)
+label3 = tk.Button(text="Cancel", command= ewin_destroy)
+label3.grid(row=1, column=1)
+
+ewin.mainloop()
