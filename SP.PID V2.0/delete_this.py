c3f1d8814c26aea2489e328c29f3ae2bb379a3b1
olisbenna <olisbenna@gmail.com>
2022-12-07 10:58:23 +0000

    g

--- SP.PID V2.0/delete_this.py
+++ SP.PID V2.0/delete_this.py
@@ -1,38 +1,5 @@
-from tkinter import *
-import tkinter as tk
-from unittest import TextTestRunner
-import matplotlib
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.gridspec import GridSpec
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-import random
-import sys
-import time
-from threading import Thread
-from PIL import ImageTk, Image
-import csv
-import shutil 
-from datetime import datetime
-from itertools import zip_longest
-import os
+g = "10 ble blu"
 
-def ewin_retry():
-    pass
-
-def ewin_destroy():
-    pass
-
-ewin = tk.Tk()
-ewin.columnconfigure([0,1], minsize=250)
-ewin.rowconfigure([0, 1], minsize=100)
-ewin.eval('tk::PlaceWindow . center')
-label1 = tk.Label(text="USB not found", font=("Helvetica", 20))
-label1.grid(row=0, column=0, columnspan=2)
-
-label2 = tk.Button(text="retry", command=ewin_retry)
-label2.grid(row=1, column=0)
-label3 = tk.Button(text="Cancel", command= ewin_destroy)
-label3.grid(row=1, column=1)
-
-ewin.mainloop()
+print(g)
+g= g.split(" ")
+print(int(g[1]))
